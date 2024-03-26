0593220f34bf8d89e73d4243a7e1b943091fda8f
ShivamTiwary000 <153612166+ShivamTiwary000@users.noreply.github.com>
2024-03-26 14:32:10 +0530

    Add files via upload

---
+++ Assingment01.js
@@ -0,0 +1,84 @@
+// Q1 Add two no.
+
+let num1 = 10;
+let num2 = 40;
+let x = num1+num2;
+console.log(x);
+
+// Q2 
+
+let a = 9;
+let b = 5;
+
+let con = a < 10 && a > b ;
+
+console.log(con);
+
+// Q3  condition check 
+
+// case 1
+
+let a1 = 800;
+let b1 = 900;
+
+let con1 = a1 % 10 == 0 && b1 % 10 == 0
+console.log(con1)
+
+// case 2 in Q3
+
+let a2 = 89;
+let b2 = 99;
+
+let con2 = a2 % 10 ==0 && b2 % 10 ==0;
+console.log(con2);
+
+// case 3 in Q3
+
+const a3 = 100;
+const b3 = 59;
+
+const con3 = a3 % 10 ==0 && b3 % 10 ==0;
+console.log(con3);
+
+// Q4 to print 1st digit of a 4 digit no 
+
+let x1 = 4567;
+
+let y1 = Math.floor (x1 / 1000);
+console.log(y1)
+
+// Q5 to print last digit of a 4 digit no 
+
+let x2 = 4567;
+let y2 = Math.floor(x2 % 10);
+console.log(y2);
+
+
+// Q6.........
+
+let s=20;
+let n=6;
+console.log(s%n);
+
+
+// Q7...... to multiplty two no. 
+
+let x3 = 80;
+let y3 = 10;
+console.log(x3 * y3);
+
+
+// Q8
+//  let shyam marks are ...are.
+
+sub1=50;
+sub2=70;
+sub3=80;
+
+total = sub1 + sub2 + sub3;
+avg = total/300*100
+
+console.log(total,avg);
+
+
+
